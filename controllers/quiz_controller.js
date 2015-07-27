4d4f72ac34c99627ae25dbc1bc11041dfa95c34e
Manuel Ruiz <ellolo78@gmail.com>
2015-07-27 21:10:00 +0200

    Restaurado a etiqueta Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,76 +1,3 @@
-// GET /quizes/question  
-//exports.question = function(req, res) {
-//  res.render('quizes/question', { pregunta: '¿Capital de Italia?'});
-//};
-
-// GET /quizes/answer
-//exports.answer = function(req, res) {
-//	if (req.query.respuesta === 'Roma') {
-//		res.render('quizes/answer', { respuesta: 'Correcto'});
-//	} else {
-//		res.render('quizes/answer', { respuesta: 'Incorrecto'});
-//	}
-//};
-
-//*************************************************************
-//Modificación módulo 7
-//var models = require('../models/models.js');
-
-// GET /quizes/question
-//exports.question = function(req, res) {
-//  models.Quiz.findAll().then(function(quiz) {
-//    res.render('quizes/question', { pregunta: quiz[0].pregunta});
-//  })
-//};
-
-// GET /quizes/answer
-//exports.answer = function(req, res) {
-//  models.Quiz.findAll().then(function(quiz) {
-//    if (req.query.respuesta === quiz[0].respuesta) {
-//      res.render('quizes/answer', { respuesta: 'Correcto' });
-//    } else {
-//      res.render('quizes/answer', { respuesta: 'Incorrecto'});
-//    }
-//  })
-//};
-//**************************************
-
-//*******************************************
-// Modificación módulo 7 video 4
-
-//var models = require('../models/models.js');
-
-// GET /quizes
-//exports.index = function(req, res) {
-  //models.Quiz.findAll().then(function(quizes) {
-    //res.render('quizes/index.ejs', { quizes: quizes});
-  //})
-//};
-
-// GET /quizes/:id
-//exports.show = function(req, res) {
-  //models.Quiz.findById(req.params.quizId).then(function(quiz) {
-    //res.render('quizes/show', { quiz: quiz});
- // })
-//};
-
-// GET /quizes/:id/answer
-//exports.answer = function(req, res) {
-  //models.Quiz.findById(req.params.quizId).then(function(quiz) {
-    //if (req.query.respuesta === quiz.respuesta) {
-      //res.render('quizes/answer', 
-        //         { quiz: quiz, respuesta: 'Correcto' });
-    //} else {
-      //res.render('quizes/answer', 
-        //         { quiz: quiz, respuesta: 'Incorrecto'});
-   // }
-  //})
-//};
-//****************************************************
-
-//****************************************************
-// Modificación Módulo 7 video 5
-
 var models = require('../models/models.js');
 
 // Autoload - factoriza el código si la ruta incluye :quizId
@@ -109,12 +36,5 @@ exports.answer = function(req, res) {
     resultado = 'Correcto';
   }
   res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
-<<<<<<< HEAD
-};
-=======
 };
 
-    Status API Training Shop Blog About Help 
-
-    © 2015 GitHub, Inc. Terms Privacy Security
->>>>>>> bbfa29b6b7a513fd331328d4f6546905cbb231eb
