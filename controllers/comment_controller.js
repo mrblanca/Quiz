3779dfe73dbae01b03fa3f6314763218d7c2bbdf
Manuel Ruiz <ellolo78@gmail.com>
2015-08-20 21:24:02 +0200

    Moderación de Comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,19 @@
 var models = require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId){
+  models.Comment.find({where: {id: Number(commentId)}}).then(
+    function(comment){
+      if (comment) {
+        req.comment = comment;
+        next();
+      } else {
+        next(new Error('No existe commentId= '+commentId));
+      }
+    }
+  ).catch(function(error){next(error)});
+};
+
 //GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
   res.render('comments/new.ejs', {quiz: req.quiz, errors: []});
@@ -19,7 +33,7 @@ exports.create = function(req, res) {
             {comment: comment, quiz: req.quiz, errors: err.errors});
       } else {
         // save: guarda en DB campo texto de comment
-        comment.save({fields: ["texto", "QuizId"]}).then(
+        comment.save({fields: ["texto", "publicado" , "QuizId"]}).then(
           // res.redirect: Redirección HTTP a la lista de preguntas
           function(){ res.redirect('/quizes/'+req.params.quizId)}
         ).catch(function(error){next(error)});
@@ -27,3 +41,12 @@ exports.create = function(req, res) {
     }
   ).catch(function(error){next(error)});
 };
+
+// GET /quizes/:quizId/comments/:commentId/publish (Esta debiera ser una operación PUT)
+exports.publish = function(req, res, next){
+  req.comment.publicado = true;
+  req.comment.save({fields: ["publicado"]}).then(
+    function(){res.redirect('/quizes/'+req.params.quizId);}
+  ).catch(function(error){next(error)});
+}; 
+
