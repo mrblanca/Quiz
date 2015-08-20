31058f29978ce8099c611d8c79e02d19c8f2880c
Manuel Ruiz <ellolo78@gmail.com>
2015-08-20 22:32:14 +0200

    Auto-logout de sesión

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Controlar la caducidad de la sesión
+app.use(function(req, res, next) {
+  var now = new Date().getTime();
+  if (req.session.lastInteraction && now-req.session.lastInteraction>120000) {
+    delete req.session.user;
+  }
+  req.session.lastInteraction = now;
+  next();
+});
+
 // Helpers dinamicos:
 app.use(function(req, res, next) {
   //Guardar path en session.redir para después de login
